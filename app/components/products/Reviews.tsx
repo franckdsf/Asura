a03d377908c823edd68f9e295308108b647540a9
Franck Desfrançais <f.desfrancais@gmail.com>
2023-12-05 14:04:54 +0100

    (feat) loox integration

--- app/components/products/Reviews.tsx
+++ app/components/products/Reviews.tsx
@@ -1,10 +1,21 @@
-import { Script } from "@shopify/hydrogen"
+import { Script, parseGid } from "@shopify/hydrogen"
+import { useEffect } from "react"
 
 export const Reviews = ({ productId, storeDomain }: { productId: string, storeDomain: string }) => {
+  useEffect(() => {
+    const t = setInterval(() => {
+      const iframe = document.querySelector('#looxReviewsFrame') as HTMLIFrameElement;
+      if (iframe)
+        iframe.style.minHeight = '100vh';
+    }, 1000)
+    return () => clearInterval(t);
+  }, [])
+
   return (
-    <div>
+    <div className="py-32 my-8 border border-neutral-500">
       <Script async src={`https://loox.io/widget/loox.js?shop=${storeDomain}`} />
-      <div id="looxReviews" data-product-id={productId} />
+      <p>Loox integration ⬇️</p>
+      <div id="looxReviews" data-product-id={parseGid(productId)} />
     </div>
   )
 }

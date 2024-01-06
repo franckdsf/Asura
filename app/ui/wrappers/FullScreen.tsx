4bd96b44c1773fe63e1465fa68f6bf2bfa018699
Franck Desfrançais <f.desfrancais@gmail.com>
2024-01-06 18:01:45 +0100

    (feat) upgraded Full Screen Component
    Now closes the component when clicking somewhere that is not a button

--- app/ui/wrappers/FullScreen.tsx
+++ app/ui/wrappers/FullScreen.tsx
@@ -22,7 +22,19 @@ export const FullScreen = ({ children, onClose }: Props) => {
 
   }, [onClose])
 
-  return createPortal(<div className="fixed top-0 left-0 w-screen h-screen bg-white animate-fadeIn" style={{ zIndex: 999 }}>
+  // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+  return createPortal(<div className="fixed top-0 left-0 w-screen h-screen bg-white animate-fadeIn" style={{ zIndex: 999 }} onClick={(e) => {
+    // get the list of all parents of the target until this current element
+    const parents = [];
+    let current = e.target as HTMLElement | null;
+    while (current) {
+      parents.push(current);
+      current = (current as HTMLElement).parentElement;
+    }
+    // if clicking on a button or a child of a button, cancel close
+    if (parents.some((el) => el instanceof HTMLButtonElement)) return;
+    onClose && onClose();
+  }}>
     {children}
   </div>, document.body)
 }
